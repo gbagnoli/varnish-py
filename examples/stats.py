d6a370354df96254408879cd8c810d6d43b859be
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-03-24 18:43:14 +0100

    Added output to stats example

--- examples/stats.py
+++ examples/stats.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+""" Example script that shows how to access the stats of a varnish server
+    This will connect to the default instance
+
+    Example output (on a test instance):
+
+$ python examples/stats.py
+All:
+[2012/03/24: 17:41:11] HIT: 834252, MISS: 45916, TOTAL: 1071713 - 216 stats read
+[2012/03/24: 17:41:12] HIT: 834266, MISS: 45916, TOTAL: 1071738 - 216 stats read
+[2012/03/24: 17:41:13] HIT: 834302, MISS: 45916, TOTAL: 1071788 - 216 stats read
+Filtered:
+[2012/03/24: 17:41:14] HIT: 834328, MISS: 45916, TOTAL: 1071825 - 3 stats read
+[2012/03/24: 17:41:15] HIT: 834331, MISS: 45916, TOTAL: 1071830 - 3 stats read
+[2012/03/24: 17:41:16] HIT: 834335, MISS: 45916, TOTAL: 1071835 - 3 stats read
+"""
+
 import time
 import varnish
 
@@ -24,6 +40,7 @@ if __name__ == '__main__':
         # using the context manager to avoid explicit init()/close()
         with varnish.Instance() as v:
 
+            print "All:"
             for i in xrange(3):
                 print_totals(v)
                 time.sleep(1)
@@ -32,7 +49,8 @@ if __name__ == '__main__':
             v.stats.include('cache_hit')\
                    .include('cache_miss')\
                    .include('client_req')
-        
+
+            print("Filtered:")
             for i in xrange(3):
                 print_totals(v)
                 time.sleep(1)
